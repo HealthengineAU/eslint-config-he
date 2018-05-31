26150da62d19ab246e8bc542927fb1d7afa42055
hen <hello@hen.zone>
2018-05-31 13:09:22 +0800

    Make the config follow its own rules, and embed prettier config here

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "parserOptions": {
-      "ecmaVersion": 7,
-      "sourceType": "module",
-      "ecmaFeatures": {
-        "experimentalObjectRestSpread": true,
-        "jsx": true
-      }
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-    "extends": [
-      "airbnb",
-      "prettier"
+  },
+  extends: [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    mocha: true,
+    jest: true,
+  },
+  plugins: ['flowtype'],
+  rules: {
+    'react/require-default-props': 0,
+    'react/forbid-prop-types': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 1,
+    'prettier/prettier': [
+      2,
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
     ],
-    "env": {
-      "es6": true,
-      "browser": true,
-      "node": true,
-      "mocha": true,
-      "jest": true
-    },
-    "plugins": [
-      "prettier"
-    ],
-    "rules": {
-      "react/require-default-props": 0,
-      "react/forbid-prop-types": 0,
-      "react/prefer-stateless-function": 0,
-      "react/jsx-filename-extension": 0,
-      "func-names": 0,
-      "no-else-return": 0,
-      "one-var": 0,
-      "import/first": 1,
-      "react/prop-types": 1,
-      "no-param-reassign": [
-        2,
-        {
-          "props": false
-        }
-      ],
-      "prettier/prettier": "error"
-    }
-  }
+  },
+};
