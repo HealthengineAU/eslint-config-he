09fc41acdea39cdaa673bec58503ee77066a48d0
hen <hello@hen.zone>
2018-05-31 14:17:02 +0800

    Normalise linter config

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ module.exports = {
         jsx: true,
       },
     },
-
+  
     extends: [
       // Airbnb provides almost all our linting rules
       'airbnb',
-
+  
       // Flowtype allows us to use flow type declarations
       'plugin:flowtype/recommended',
-
+  
       // Prettier provides opinionated defaults for all code-layout choices
       'plugin:prettier/recommended',
     ],
-
+  
     // Allows assuming the presence of globals provided by all these environments
     env: {
       es6: true,
@@ -29,10 +29,12 @@ module.exports = {
       mocha: true,
       jest: true,
     },
-
-    // Allows linting type-declarations
-    plugins: ['flowtype'],
-
+  
+    plugins: [
+        // Allows linting type-declarations
+        'flowtype',
+    ],
+  
     rules: {
       // Downgrades these accessibility rules to warnings while we decide on policy
       "jsx-a11y/interactive-supports-focus": 1,
@@ -44,28 +46,31 @@ module.exports = {
       
       // Allows importing from files with explicit '.js' extensions
       'import/extensions': 0,
-
+  
       // Allows default props to be set when destructuring
       'react/require-default-props': 0,
-
+  
       // Allows using object/any as prop types
       'react/forbid-prop-types': 0,
-
+  
       // Class-based components are still needed when using refs
       'react/prefer-stateless-function': 0,
-
+  
       // Permits normal '.js' files to use inline JSX
       'react/jsx-filename-extension': 0,
-
+  
       // Permits defining proptypes in flow instead
       'react/prop-types': 1,
-
+  
       // Downgrades bad array keys to a warning
       'react/no-array-index-key': 1,
-
+  
       // Downgrades unescaped HTML entity literals to a warning 
       'react/no-unescaped-entities': 1,
-
+  
+      // Prevent assigning to arguments, but allow modifying them.
+      "no-param-reassign": [2, {props: false}],
+  
       // Applies our code-layout customizations
       'prettier/prettier': [
         2,
